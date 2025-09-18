7778cae100ebf7f21f88955e291a4be0c958e24a
Daniel Ojeda Balsera <danojeda205@gmail.com>
2025-09-18 14:50:10 +0200

    cosas de py e js

---
+++ Cliente/ejemplo1/ejemplo1.js
@@ -0,0 +1,8 @@
+//alert("Hola mundo desde alerta !");console.log("Hola mundo consola");
+//document.getElementById("salida").innerHTML="Hola mundo";
+//alert("El doble de "+num + " es "+resultado)
+
+let num =prompt("Dime un numero: ");
+let resultado =num*2;
+document.getElementById("salida").innerHTML=
+    "El doble de "+num + " es "+resultado;
