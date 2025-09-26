3752ff50e083bf25403317cd8f30022a35539bf2
xdojebal477-hub <xdojebal477@ieshnosmachado.org>
2025-09-26 14:53:14 +0200

    terminar boletin2.js

---
+++ Cliente/boletin2/boletin2.js
@@ -0,0 +1,3 @@
+let cadena = prompt("Introduce una cadena de texto:");
+let cadenaFinal=cadena.trim();
+document.getElementById("resultado").innerHTML = "Tu nombre y apellidos mide "+ cadenaFinal.length + " caracteres.";
