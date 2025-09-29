645e78c05296305c1cef7af4f7631c0c259aea4f
xdojebal477-hub <xdojebal477@ieshnosmachado.org>
2025-09-29 14:55:13 +0200

    terminar boletin 2 js

--- Cliente/boletin2/boletin2.js
+++ Cliente/boletin2/boletin2.js
@@ -1,3 +1,5 @@
 let cadena = prompt("Introduce una cadena de texto:");
 let cadenaFinal=cadena.trim();
 document.getElementById("resultado").innerHTML = "Tu nombre y apellidos mide "+ cadenaFinal.length + " caracteres.";
+document.getElementById("resultado2").innerHTML = "Tu nombre y apellidos en mayúsculas es: "+ cadenaFinal.toUpperCase() + ".";
+document.getElementById("resultado2").innerHTML = "Tu nombre y apellidos en minisculas  es: "+ cadenaFinal.toLowerCase + ".";
--- Servidor/pruebas.py
+++ Servidor/pruebas.py
@@ -1,3 +1,39 @@
 
 
 
+accion = input("Que quieres hacer (crear/baja/asc/desc/salir): ").lower()
+lista_alumnos = []
+
+while accion != "salir":
+    match accion:
+        case "crear":
+            alumno = input("Introduce un alumno (escribe 'fin' para terminar): ")
+            while alumno.lower() != "fin":
+                if alumno in lista_alumnos:
+                    print("Alumno ya introducido")
+                else:
+                    lista_alumnos.append(alumno)
+                alumno = input("Introduce un alumno (escribe 'fin' para terminar): ")
+            print("Lista actual de alumnos:", lista_alumnos)
+            
+        case "baja":
+            alumno = input("Introduce un alumno para eliminar: ")
+            if alumno in lista_alumnos:  # Evitar error si no existe
+                lista_alumnos.remove(alumno)
+                print(f"Alumno '{alumno}' eliminado")
+            else:
+                print("Alumno no encontrado")
+        
+        case "asc":
+            lista_alumnos_ordenados = sorted(lista_alumnos)
+            print("Lista ordenada ascendente:", lista_alumnos_ordenados)
+        
+        case "desc":
+            lista_alumnos_ordenados = sorted(lista_alumnos, reverse=True)
+            print("Lista ordenada descendente:", lista_alumnos_ordenados)
+        
+        case _:  # Opción no válida
+            print("Opción no válida")
+    
+    # Pedimos la acción nuevamente al final del bucle
+    accion = input("Que quieres hacer (crear/baja/asc/desc/salir): ").lower()
