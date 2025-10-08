361bcc05ad0fe10dca71894f20d15d7cdb44430e
Daniel Ojeda Balsera <danojeda205@gmail.com>
2025-10-08 23:24:18 +0200

    terminar ej15js

--- Cliente/Boletín2/Boletín2/ej15.js
+++ Cliente/Boletín2/Boletín2/ej15.js
@@ -1,4 +1,11 @@
 function generarLetraDNI(){
     let dnis=[]
-    
+    let intervalo=setInterval(()=>{
+        let dni=input("Introduce un DNI (sin letra, -1 para terminiar): ")
+        if(dni===-1){
+            clearInterval(intervalo)
+        }else{
+            dnis.push(dni)
+        }
+    },20000)
 }
