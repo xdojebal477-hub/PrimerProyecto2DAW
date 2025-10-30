be3353101f627e63ec4ea467e262b4ea5bab675d
xdojebal477-hub <xdojebal477@ieshnosmachado.org>
2025-10-30 12:12:16 +0100

    .py

--- primerproyecto/blog/models.py
+++ primerproyecto/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo=models.CharField(max_length=200)
+    autor=models.CharField(max_length=60)
+    cuerpo=models.TextField()
+    email=models.EmailField(default='dani@gmail.com')
+
+    
+    def __str__(self):
+        return f"({self.id}){self.titulo}"
+    
