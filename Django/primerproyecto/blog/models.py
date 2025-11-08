004cec5a26c60938bbb5657dcdade0019a41dc2d
Daniel Ojeda Balsera <danojeda205@gmail.com>
2025-11-08 17:13:31 +0100

    blog/,admin/ hechos. Modelos Post migrados en la bbdd

--- Django/primerproyecto/blog/models.py
+++ Django/primerproyecto/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+        titulo=models.CharField(max_length=200)
+        autor=models.CharField(max_length=200)
+        cuerpo=models.TextField()
+        email=models.EmailField(default='user@user.es')
+        
+        def  __str__(self):
+            return self.titulo
+        
