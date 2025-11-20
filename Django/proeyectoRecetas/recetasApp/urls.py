472c0ad83212c2a73a28805be5649aafa4fa82e4
Daniel Ojeda Balsera <danojeda205@gmail.com>
2025-11-20 21:50:38 +0100

    App recetasWeb con crud

--- Django/proeyectoRecetas/recetasApp/urls.py
+++ Django/proeyectoRecetas/recetasApp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('ingredientes/', views.ingredientes_list, name='ingredientes_list'),]
+    path('ingredientes/', views.ingredientes_list, name='ingredientes_list'),
+    path('ingredientes/crear/',views.crear_ingrediente,name='crear_ingrediente'),
+    path('ingredientes/<int:pk>/', views.ingrediente_detalle, name='ingrediente_detalle'),
+    path('ingredientes/<int:pk>/editar/',views.ingrediente_editar,name='ingrediente_editar'),
+    path('ingredientes/<int:pk>/eliminar/',views.ingrediente_eliminar,name='ingrediente_eliminar'),
+    ]
--- Django/proeyectoRecetas/recetasApp/views.py
+++ Django/proeyectoRecetas/recetasApp/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
 
-from Django.proeyectoRecetas.recetasApp.forms import FiltroIngredienteForm
+from .forms import FiltroIngredienteForm
 from .models import Ingrediente,CategoriaIngrediente
 # Create your views here.
 
 def inicio(request):
 
-    return render(request, 'recetasApp/inicio.html')    
+    return render(request, 'recetasApp/inicio.html')
 
 
 def ingredientes_list(request):
@@ -19,9 +19,51 @@ def ingredientes_list(request):
     if  categoria_filtro:
         ingredientes=ingredientes.filter(categoria__pk=categoria_filtro)
     if  lactosa_filtro:
-        ingredientes=ingredientes.filter(lactosa=True)
+        ingredientes=ingredientes.filter(lactosa__lactosa=True)
     if cantidad_filtro:
         ingredientes=ingredientes.filter(cantidad__cantidad=cantidad_filtro)
     
     formulario_filtro=FiltroIngredienteForm()
     return render(request, 'recetasApp/ingredientes_list.html',{'ingredientes':ingredientes, 'categorias': categorias,'lactosa_filtro': lactosa_filtro, 'cantidad_filtro': cantidad_filtro, 'formulario_filtro': formulario_filtro}) 
+
+
+def crear_ingrediente(request):
+    if request.method == 'POST':
+        # Creamos el formulario con los datos enviados
+        form = FiltroIngredienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ingredientes_list')
+    else:
+        # Si es GET, mostramos el formulario vacío
+        form = FiltroIngredienteForm()
+    
+    return render(request, 'recetasApp/ingrediente_form.html', {'form': form})
+
+
+def ingrediente_detalle(request, pk):
+    
+    ingrediente = get_object_or_404(Ingrediente, pk=pk)
+    return render(request, 'recetasApp/ingrediente_detalle.html', {'ingrediente': ingrediente})
+
+
+def ingrediente_editar(request, pk):
+    
+    ingrediente=get_object_or_404(Ingrediente,pk=pk)
+    if request.method =='POST':
+        form=FiltroIngredienteForm(request.POST,instance=ingrediente)
+        if form.is_valid():
+            form.save()
+            return redirect('ingredientes_list')
+    else:
+        form=FiltroIngredienteForm(instance=ingrediente)
+    return render(request,'recetasApp/ingrediente_form.html',{'form':form,'ingrediente':ingrediente})
+
+def ingrediente_eliminar(request, pk):
+    
+    ingrediente=get_object_or_404(Ingrediente,pk=pk)
+    if request.method=='POST':
+        ingrediente.delete()
+        return redirect('ingredientes_list')
+    else:
+        return render(request,'recetasApp/ingrediente_eliminar.html',{'ingrediente':ingrediente})
